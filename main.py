35acdfe65e3b6c2ac62a2e960c9f168b162dfbb3
moe93 <m.odeh93@gmail.com>
2020-04-30 16:18:18 -0400

    Draw cards, mulligan, and generate hand functions

--- main.py
+++ main.py
@@ -4,10 +4,12 @@
 * land drop each turn for a 60 cards deck...also avoid
 * getting mana flooded/fucked, that ain't cool
 *
-* VERSION: 0.05a
+* VERSION: 0.07a
 *   - ADDED   : Hi!
 *   - ADDED   : Mana curve plot
 *   - ADDED   : Deck generator
+*   - ADDED   : Hand + card draw (by extension, a 7 card starting hand)
+*   - ADDED   : Implement mulligan rule
 *
 *
 * KNOWN ISSUES:
@@ -16,7 +18,7 @@
 *
 * AUTHOR                    :   Mohammad Odeh
 * DATE                      :   Apr. 26th, 2020 Year of the COVID
-* LAST CONTRIBUTION DATE    :   Apr. 28th, 2020 Year of the COVID
+* LAST CONTRIBUTION DATE    :   Apr. 30th, 2020 Year of the COVID
 *
 '''
 
@@ -102,7 +104,15 @@ class MTG_mana( object ):
             raise ValueError( "Card totals do NOT match!" )             # ...
 
 ##        self.compute_curve()                                            # Compute the mana curve
+
+        self.hand = list()                                              # Create an empty hand variable
+        self.mulligan_attempts = 0                                      # How many times we mulliganed
+        
         self.make_deck()                                                # Create a deck and shuffle it
+        self.draw_card( init=True )                                     # Draw starting hand
+
+##        self.play_turn()                                                # Play a land and a card (if possible)
+        
 ##        self.compute_mana()                                             # Compute how many lands are needed
 
 # --------------------------
@@ -151,10 +161,10 @@ class MTG_mana( object ):
         # Prepare cards deck
         self.deck = list()                                              # Make empty deck
         for i in range( 0, len(args.cmc_color) ):                       # Loop over args.cmc_color
-            [num, cmc] = args.cmc_color[i].split('x')                   # Extract #cards, cmc
+            [num, cmc] = args.cmc_color[i].split('x')                   #   Extract #cards, cmc
 
-            for j in range( 0, int(num) ):                              # Append that many # of cards
-                self.deck.append( cmc )                                 # of specifc cmc to deck
+            for j in range( 0, int(num) ):                              #   Append that many # of cards
+                self.deck.append( cmc )                                 #   of specifc cmc to deck
 
         if( len(self.deck) != args.non_land ):                          # Make sure card numbers match
             raise ValueError( "Card totals do NOT match!" )             # ...
@@ -163,12 +173,12 @@ class MTG_mana( object ):
         # Generate lands, for now use 1xLand for 2xColor
         self.land_cards = list()                                        # Create empty lands deck (temp)
         
-        for i in range( 0, len(args.mana_color) ):
+        for i in range( 0, len(args.mana_color) ):                      # Find out land color requirements from input
             # FROM: https://stackoverflow.com/questions/46418265/how-to-count-the-number-of-times-a-specific-character-appears-in-a-list
             total_mana = sum( s.count(args.mana_color[i]) for s in self.deck )
 
             for j in range( 0, -(-total_mana//2) ):                     # -(-x//y) rounds up
-                self.land_cards.append( args.mana_color[i]+'L' )        # Generate list of lands
+                self.land_cards.append( args.mana_color[i]+'L' )        #   Generate list of lands
 
 
         # Now shuffle deck of cards + lands
@@ -176,8 +186,64 @@ class MTG_mana( object ):
         self.deck.extend( self.land_cards )                             # Add lands to main deck
         shuffle( self.deck )                                            # Shuffle deck
 
-        if( args.dev_mode ): print( self.deck )                         # Print
+        if( args.dev_mode ): print( self.deck )                         # [INFO] Print deck
 
+# --------------------------
+
+    def draw_card( self, init=False ):
+        '''
+        Draw 1 card
+
+        NOTE:
+            - if( init==True ): draw 7 cards
+        '''
+
+        if( init ):                                                     # If initial draw
+            for i in range( 0, 7-self.mulligan_attempts ):              #   Draw (7-mulligan_attempts) cards
+                self.hand.append( self.deck.pop() )                     #       Pop first card from deck into hand
+            if( args.dev_mode ): print( self.hand )                     # [INFO] Print hand
+            
+            num_lands = sum( s.count('L') for s in self.hand )          # Check hand for # of lands
+            if( num_lands == len(self.hand)-2 or num_lands < 2 ):       # If too many\few lands, mulligan
+                self.mulligan()                                         #   Mulligan
+                
+        else:                                                           # Else
+            self.hand.append( self.deck.pop() )                         #   Draw 1 card
+
+# --------------------------
+
+    def mulligan( self ):
+        '''
+        Return hand to deck, shuffle, then draw one less card
+        '''
+
+        self.deck.extend( self.hand )                                   # Return hand to deck
+        self.hand.clear()                                               # Clear hand
+        shuffle( self.deck )                                            # Shuffle deck once more
+
+        self.mulligan_attempts += 1                                     # One less card in starting hand
+        if( args.dev_mode ): print( "[INFO] Mulliganing {}".format(self.mulligan_attempts) )         # [INFO] ...
+        
+        if( self.mulligan_attempts == 7 ):                              # Check if we exceeded number of mulligan attempts
+            print( "Maximum mulligan attempts reached. Quitting!" )     #   [INFO] ...
+            quit()                                                      #   Quit program
+            
+        else:                                                           # If not
+            self.draw_card( init=True )                                 #   Draw hand with (mulligan_attempts) less cards
+
+### --------------------------
+##
+##    def play_turn( self ):
+##        '''
+##        1. Check hand for lands
+##        2. Determine # of cards in hand with
+##            CMC that are playable this turn.
+##        3. Play land card if possible
+##        4. Else
+##        '''
+##
+##        quit()
+            
 ### --------------------------
 ##
 ##    def compute_mana( self ):
@@ -186,13 +252,6 @@ class MTG_mana( object ):
 ##
 ##        NOTE:
 ##            - Doesn't take into consideration mana-fixing cards
-##        '''
-##
-### --------------------------
-##
-##    def draw_card( self ):
-##        '''
-##        Draw 1 card
 ##        '''
 ##
 
